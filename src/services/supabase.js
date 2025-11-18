e0902ea0ea362f97738e47c4e6e9f915b0b889e2
launick9 <alanlautaroarroyo@buenosaires.gob.ar>
2025-11-18 12:53:31 -0300

    fix: volver a ocultar claves de supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://hwxfavsaimbpfbnlvnqw.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3eGZhdnNhaW1icGZibmx2bnF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1NTY1NjcsImV4cCI6MjA3NDEzMjU2N30.-PQvTcGoB13nPZ2YcM32naKoTeljxM-Y6kOIZRm9w0E';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
